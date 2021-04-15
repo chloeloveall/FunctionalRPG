cc41a79c80967fb9651c1a547a217b726d462d3c
Nick Lindau <nicholaithegreat@gmail.com>
2021-04-15 16:56:33 -0700

    created a bunch of fun stuff

--- src/main.js
+++ src/main.js
@@ -1,64 +1,85 @@
-/*eslint-disable*/
 import $ from 'jquery';
-/*eslint-disable*/
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 export const storeState = (initialState = {}) => {
   let currentState = initialState;
   return (stateChangeFunction = state => state) => {
     const newState = stateChangeFunction(currentState);
-    currentState = {...newState};
+    currentState = { ...newState };
     return newState;
-  }
-}
+  };
+};
 
-// const stateControl = storeState();
-const warrior = {strength: 5, dexterity: 3, intelligence: 1}
-const wizard = {strength: 2, dexterity: 1, intelligence: 6};
-const thief = {strength: 3, dexterity: 5, intelligence: 1};
+let stateControl = storeState();
+const warrior = { strength: 5, dexterity: 3, intelligence: 1, hitPoints: 100, experiencePoints: 0, level: 1 };
+const wizard = { strength: 2, dexterity: 1, intelligence: 6, hitPoints: 100, experiencePoints: 0, level: 1 };
+const thief = { strength: 3, dexterity: 5, intelligence: 1, hitPoints: 100, experiencePoints: 0, level: 1 };
 
 export const changeState = (prop) => {
   return (value) => {
-    return (state) =>  ({
+    return (state) => ({
       ...state,
-      [prop] : (state[prop] || 0) + value
-    }) 
-  }
-}
+      [prop]: (state[prop] || 0) + value
+    });
+  };
+};
 
-const plusStrength = changeState("strength")(1);
+// const plusStrength = changeState("strength")(1);
+// const plusIntelligence = changeState("intelligence")(1);
+// const plusDexterity = changeState("dexterity")(1);
+const addXP = changeState("experiencePoints")(50);
+const addHP = changeState("hitPoints")(15);
+const lowerHP = changeState("hitPoints")(-15);
+const learnFireball = changeState("fireballLevel")(1);
+// const levelUp = changeState("level")(1);
 
-$(document).ready(function() {
-$("#buttonForWarrior").click(function()  {
-  event.preventDefault();
-  const stateControl = storeState(warrior);
-  console.log(stateControl());
-  $(".heroSelect").hide();
-  $(".hero").show();
-  $(".stats").text(`Strength: ${newState.strength} Dexterity: ${newState.dexterity} Intelligence: ${newState.intelligence}`);
-})
-$("#buttonForWizard").click(function()  {
-  event.preventDefault();
-  const stateControl = storeState(wizard);
-  console.log(stateControl());
-  $(".heroSelect").hide();
-  $(".hero").show();
-})
-$("#buttonForThief").click(function()  {
-  event.preventDefault();
-  const stateControl = storeState(thief);
-  console.log(stateControl());
-  $(".heroSelect").hide();
-  $(".hero").show();
-})
-
-// This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
-
-  // $('#plusStrength').click(function() {
-  //   const newState = stateControl(plusStrength);
-  //   $('#soil-value').text(`Soil: ${(newState.soil === undefined) ? 0 : newState.soil} Water: ${(newState.water === undefined) ? 0 : newState.water}`);
-  // });
-
+$(document).ready(function () {
+  $("#buttonForWarrior").click(function () {
+    stateControl = storeState(warrior);
+    console.log(stateControl());
+    $(".heroSelect").hide();
+    $(".hero").show();
+    $(".heroLevel").append(`Level: ${stateControl().level}`);
+    $(".stats").append(` Strength: ${stateControl().strength} Dexterity: ${stateControl().dexterity} Intelligence: ${stateControl().intelligence} Hit Points: ${stateControl().hitPoints} Experience Points: ${stateControl().experiencePoints}`);
+    return stateControl;
+  });
+  $("#buttonForWizard").click(function () {
+    stateControl = storeState(wizard);
+    console.log(stateControl());
+    $(".heroSelect").hide();
+    $(".hero").show();
+    $(".heroLevel").append(`Level: ${stateControl().level}`);
+    $(".stats").append(`Strength: ${stateControl().strength} Dexterity: ${stateControl().dexterity} Intelligence: ${stateControl().intelligence} Hit Points: ${stateControl().hitPoints} Experience Points: ${stateControl().experiencePoints}`);
+  });
+  $("#buttonForThief").click(function () {
+    stateControl = storeState(thief);
+    console.log(stateControl());
+    $(".heroSelect").hide();
+    $(".hero").show();
+    $(".heroLevel").append(`Level: ${stateControl().level}`);
+    $(".stats").append(`Strength: ${stateControl().strength} Dexterity: ${stateControl().dexterity} Intelligence: ${stateControl().intelligence} Hit Points: ${stateControl().hitPoints} Experience Points: ${stateControl().experiencePoints}`);
+  });
+  $("#AddXP").click(function () {
+    stateControl(addXP);
+    $(".stats").text(`Strength: ${stateControl().strength} Dexterity: ${stateControl().dexterity} Intelligence: ${stateControl().intelligence} Hit Points: ${stateControl().hitPoints} Experience Points: ${stateControl().experiencePoints}`);
+    console.log(stateControl());
+    console.log(stateControl().experiencePoints)
+  });
+  $("#AddHP").click(function () {
+    stateControl(addHP);
+    $(".stats").text(`Strength: ${stateControl().strength} Dexterity: ${stateControl().dexterity} Intelligence: ${stateControl().intelligence} Hit Points: ${stateControl().hitPoints} Experience Points: ${stateControl().experiencePoints}`);
+    console.log(stateControl());
+  });
+  $("#LowerHP").click(function () {
+    stateControl(lowerHP);
+    $(".stats").text(`Strength: ${stateControl().strength} Dexterity: ${stateControl().dexterity} Intelligence: ${stateControl().intelligence} Hit Points: ${stateControl().hitPoints} Experience Points: ${stateControl().experiencePoints}`);
+    console.log(stateControl());
+  });
+  $("#LearnFireball").click(function () {
+    stateControl(learnFireball);
+    $(".fireballLevel").text(`Fireball Level: ${stateControl().fireballLevel}`)
+    console.log(stateControl());
+  });
 });
